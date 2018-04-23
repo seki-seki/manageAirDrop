c3b6604855e7b4a54de400568268491753137cce
seki <schokolade0317@gmail.com>
2018-04-23 22:25:54 +0900

    Enable show list

--- src/js/app.js
+++ src/js/app.js
@@ -31,31 +31,59 @@ App = {
         airDropsRow.empty()
         App.contracts.AirDrop.deployed().then(function (instance) {
             AirDropInstance = instance;
-            return AirDropInstance.getExpireContentsIndexes.call();
-        }).then(function (indexes) {
-            indexes.forEach(function (i) {
-                AirDropInstance.airDropContents(i).then(function(airDropContent) {
-                    //TODO: I want recieve airDropContent as map instead of array to access using domain name
-                    airDropsRow.append(
-                        `<div class = "airDrop">
-                         <ul>
-                            <li>Name: ${web3.toAscii(airDropContent[1])}</li>
-                            <li>Symbol: ${web3.toAscii(airDropContent[2])}</li>
-                            <li>Adress: ${airDropContent[0]}</li>
-                            <li>Image: ${airDropContent[3]}</li>
-                            <li>WebSiteUrl: ${airDropContent[4]}</li>
-                            <li>Descriptions: ${airDropContent[5]}</li>
-                            <li>Decimal: ${airDropContent[6]}</li>
-                            <li>TotalSupply;: ${airDropContent[9]}</li>
-                            <li>StartDate: ${new Date(airDropContent[7]*1000)}</li>
-                            <li>ExpireDate: ${new Date(airDropContent[8]*1000)}</li>
-                        </ul>
-                    </div>`)
-                })
-            })
+            //List Expire Contents
+            AirDropInstance.getExpireContentsIndexes.call().then(function (indexes) {
+                    indexes.forEach(function (i) {
+                        AirDropInstance.airDropContents(i).then(function (airDropContent) {
+                            //TODO: I want recieve airDropContent    as map instead of array to access using domain name
+                            airDropsRow.append(
+                                `<div class = "airDrop">
+                                     <ul>
+                                        <li>Name: ${web3.toAscii(airDropContent[1])}</li>
+                                        <li>Symbol: ${web3.toAscii(airDropContent[2])}</li>
+                                        <li>Adress: ${airDropContent[0]}</li>
+                                        <li>Image: ${airDropContent[3]}</li>
+                                        <li>WebSiteUrl: ${airDropContent[4]}</li>
+                                        <li>Descriptions: ${airDropContent[5]}</li>
+                                        <li>Decimal: ${airDropContent[6]}</li>
+                                        <li>TotalSupply;: ${airDropContent[9]}</li>
+                                        <li>StartDate: ${new Date(airDropContent[7] * 1000)}</li>
+                                        <li>ExpireDate: ${new Date(airDropContent[8] * 1000)}</li>
+                                    </ul>
+                                </div>`)
+                        })
+                    })
+                }
+            );
+            // List Own Contents
+            let ownContentsRow = $('#ownContentsRow');
+            ownContentsRow.empty()
+            AirDropInstance.getOwnContentsIndexes.call().then(function (indexes) {
+                    indexes.forEach(function (i) {
+                        AirDropInstance.airDropContents(i).then(function (airDropContent) {
+                            //TODO: I want recieve airDropContent    as map instead of array to access using domain name
+                            ownContentsRow.append(
+                                `<div class = "airDrop">
+                                     <ul>
+                                        <li>Name: ${web3.toAscii(airDropContent[1])}</li>
+                                        <li>Symbol: ${web3.toAscii(airDropContent[2])}</li>
+                                        <li>Adress: ${airDropContent[0]}</li>
+                                        <li>Image: ${airDropContent[3]}</li>
+                                        <li>WebSiteUrl: ${airDropContent[4]}</li>
+                                        <li>Descriptions: ${airDropContent[5]}</li>
+                                        <li>Decimal: ${airDropContent[6]}</li>
+                                        <li>TotalSupply;: ${airDropContent[9]}</li>
+                                        <li>StartDate: ${new Date(airDropContent[7] * 1000)}</li>
+                                        <li>ExpireDate: ${new Date(airDropContent[8] * 1000)}</li>
+                                    </ul>
+                                </div>`)
+                        })
+                    })
+                }
+            );
         }).catch(function (err) {
             console.log(err.message);
-        });
+        })
     },
 
 };
